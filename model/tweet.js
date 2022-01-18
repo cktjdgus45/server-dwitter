c85ab72628078934200eee14a1043332d3411e84
pooh <cktjdgus45@naver.com>
2022-01-18 16:33:24 +0900

    add model to server

---
+++ model/tweet.js
@@ -0,0 +1,51 @@
+let tweets = [
+    {
+        id: '1',
+        text: '화이팅',
+        createdAt: Date.now().toString(),
+        name: 'Bob',
+        username: 'bob',
+        url: ''
+    },
+    {
+        id: '2',
+        text: '화이팅',
+        createdAt: Date.now().toString(),
+        name: 'Paul',
+        username: 'paul',
+        url: ''
+    },
+];
+
+export function getAll() {
+    return tweets;
+}
+export function getAllByUsername(username) {
+    return tweets.filter(tweet => tweet.username === username);
+}
+export function getAllById(id) {
+    return tweets.find(tweet => tweet.id === id);
+}
+export function create(text, username, name) {
+    const tweet = {
+        id: Date.now().toString(),
+        text,
+        createdAt: new Date(),
+        name,
+        username
+    }
+    tweets = [tweet, ...tweets];
+    return tweet;
+}
+
+export function update(id, text) {
+    const tweet = tweets.find((tweet) => tweet.id === id);
+    if (tweet) {
+        tweet.text = text;
+    }
+    console.log(id, text, tweet)
+    return tweet;
+}
+export function remove(id) {
+    tweets = tweets.filter(tweet => tweet.id !== id);
+}
