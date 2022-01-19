c302a6046b742eda521bc09fa1fef53c563c10b6
pooh <cktjdgus45@naver.com>
2022-01-19 16:05:01 +0900

    add validation to server

--- router/tweetRoute.js
+++ router/tweetRoute.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import 'express-async-errors';
 import * as tweetController from '../controller/tweetController.js';
+import { body, validationResult, param } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const tweetRouter = express.Router();
-tweetRouter.get('/', tweetController.getTweets)
+const validateTweet = [body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('text should be at least 3 characters'), validate];
 
-tweetRouter.get('/:id', tweetController.getTweet)
+tweetRouter.get('/', tweetController.getTweets);
 
-tweetRouter.post('/', tweetController.createTweet)
+tweetRouter.get('/:id', tweetController.getTweet);
 
-tweetRouter.put('/:id', tweetController.updateTweet);
+tweetRouter.post('/', validateTweet, tweetController.createTweet);
+
+tweetRouter.put('/:id', validateTweet, tweetController.updateTweet);
 
 tweetRouter.delete('/:id', tweetController.deleteTweet);
 
